131124eb768c84a70400b97a85f44905be0386b8
Dereck Ventura <dereckenriquev@gmail.com>
2026-01-29 22:47:13 -0600

    Git: Add Remote

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,6 @@
-"use strict";
-//Desarrolle un algoritmo para Estudiante que trabaje con una materia donde se puedan observar las notas
-//de laboratorio 1 y Parcial 1.
+
 Object.defineProperty(exports, "__esModule", { value: true });
+
 class Poo {
     constructor(_nombre, _codigo, _labo, _parcial) {
         this.nombre = _nombre;
@@ -14,28 +13,28 @@ class Poo {
             + " Parcial: " + this.parcial);
     }
 }
-class Logica {
-    constructor(nombre, lab1, lab2, lab3, par1, par2, par3) {
+class Redes {
+    constructor(nombre, laboratorio1, laboratorio2, laboratorio3, parcial1, parcial2, parcial3) {
         this.nombre = nombre;
-        this.lab1 = lab1;
-        this.lab2 = lab2;
-        this.lab3 = lab3;
-        this.par1 = par1;
-        this.par2 = par2;
-        this.par3 = par3;
+        this.laboratorio1 = laboratorio1;
+        this.laboratorio2 = laboratorio2;
+        this.laboratorio3 = laboratorio3;
+        this.parcial1 = parcial1;
+        this.parcial2 = parcial2;
+        this.parcial3 = parcial3;
     }
     Notafinal() {
-        let C1 = ((this.lab1 * 0.60) + (this.par1 * 0.40)) / 3;
-        let C2 = ((this.lab2 * 0.60) + (this.par2 * 0.40)) / 3;
-        let C3 = ((this.lab3 * 0.60) + (this.par3 * 0.40)) / 3;
-        let NF = C1 + C3 + C3;
-        console.log("El estudiante " + this.nombre + " su nota final de logica es: " + NF);
+        let Prom1 = ((this.laboratorio1 * 0.60) + (this.parcial1 * 0.40)) / 3;
+        let Prom2 = ((this.laboratorio2 * 0.60) + (this.parcial2 * 0.40)) / 3;
+        let Prom3 = ((this.laboratorio3 * 0.60) + (this.parcial3 * 0.40)) / 3;
+        let nota_Final = Prom1 + Prom2 + Prom3;
+        console.log("Dereck Enrique Ventura Benitez " + this.nombre + " su nota final de redes es: " + nota_Final);
     }
 }
-// let estudiante1 = new Poo('William Jose Morales', 'u20110430', 8, 9);
+let estudiante1 = new Poo('Dereck Enrique Ventura Benitez', 'u20250944', 8, 9);
 let nombre = "Chepe trompo";
-let estudiante2 = new Logica(nombre, 8, 9, 8, 10, 10, 8);
-// let estudiante3= new Poo();
-// estudiante1.mostrarDatos();
+let estudiante2 = new Redes(nombre, 8, 9, 8, 10, 10, 8);
+
+
+estudiante1.mostrarDatos();
 estudiante2.Notafinal();
-//# sourceMappingURL=index.js.map
